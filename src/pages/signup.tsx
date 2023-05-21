3d47c26eb44e7b513436129bd1700f1d850d588f
hamaiook <dososokung@gmail.com>
2023-05-21 20:59:46 +0700

    feat: add form login and signup

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,23 +1,23 @@
 import axios from 'axios'
-import Cookies from 'js-cookie'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { useRef } from 'react'
 
 import { NEXT_API_URL } from '@/environments'
 import { User } from '@/types/User'
 
-const Login = () => {
+const Signup = () => {
+  const router = useRouter()
   const emailRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
 
-  const handleLogin = async () => {
+  const handleSignup = async () => {
     if (emailRef.current?.value && passwordRef.current?.value) {
       try {
-        const { data } = await axios.post<User>(`${NEXT_API_URL}/user/login`, {
+        await axios.post<User>(`${NEXT_API_URL}/users`, {
           email: emailRef.current.value,
           password: passwordRef.current.value,
         })
-        Cookies.set('login-token', data.id.toString(), { expires: 365 })
+        router.replace('/')
       } catch (error) {}
     }
   }
@@ -27,7 +27,7 @@ const Login = () => {
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Sign Up
           </h2>
         </div>
 
@@ -75,27 +75,17 @@ const Login = () => {
             <div>
               <button
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={handleLogin}
+                onClick={handleSignup}
                 type="button"
               >
-                Sign in
+                Sign Up
               </button>
             </div>
           </form>
-
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Don’t have an account yet?
-            <Link
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              href="/signup"
-            >
-              Sign up
-            </Link>
-          </p>
         </div>
       </div>
     </main>
   )
 }
 
-export default Login
+export default Signup
