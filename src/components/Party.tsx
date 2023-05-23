1a5db3f5e2eeafc3d5ddbedeb6ee064cf277ced5
hamaiook <dososokung@gmail.com>
2023-05-23 11:15:33 +0700

    fix: config

--- src/components/Party.tsx
+++ src/components/Party.tsx
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
+import { useRouter } from 'next/router'
 import { useCallback, useEffect, useState } from 'react'
 
 import { NEXT_API_URL } from '@/environments'
 import { Party } from '@/types/Party'
 
 const PartyPage = () => {
+  const router = useRouter()
   const [listParty, setListParty] = useState<Party[]>([])
 
   const fetchParty = useCallback(async (controller: AbortController) => {
@@ -41,18 +43,31 @@ const PartyPage = () => {
     }
   }
 
+  const handleParty = () => {
+    router.push('/createparty')
+  }
+
   return (
     <div>
       <p className="flex items-center justify-center p-10 text-4xl">Party</p>
+      <div className="flex w-[500px] justify-end">
+        <button
+          className="mb-2 flex justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          onClick={handleParty}
+          type="button"
+        >
+          Add party
+        </button>
+      </div>
       {listParty.map((list) => {
         return (
           <div
-            className="mb-2 flex w-[500px] items-center justify-between rounded-lg bg-gray-200 px-4 py-4"
+            className="mb-2 flex w-[500px] items-center justify-between rounded-lg bg-gray-200 px-4 py-4 hover:bg-gray-100"
             key={list.id}
           >
             <span>{list.name}</span>
             <span
-              className="cursor-pointer rounded-lg bg-blue-200 px-4 py-2"
+              className="cursor-pointer rounded-lg bg-blue-200 px-4 py-2 hover:bg-blue-100"
               onClick={() => handleJoin(list.id)}
             >
               Join
--- src/pages/createparty.tsx
+++ src/pages/createparty.tsx
@@ -5,22 +5,20 @@ import { useRef } from 'react'
 import { NEXT_API_URL } from '@/environments'
 import { User } from '@/types/User'
 
-const Signup = () => {
+const Createparty = () => {
   const router = useRouter()
-  const emailRef = useRef<HTMLInputElement>(null)
-  const passwordRef = useRef<HTMLInputElement>(null)
+  const nameRef = useRef<HTMLInputElement>(null)
 
-  const handleSignup = async () => {
-    if (emailRef.current?.value && passwordRef.current?.value) {
+  const handleParty = async () => {
+    if (nameRef.current?.value) {
       try {
-        await axios.post<User>(`${NEXT_API_URL}/users`, {
-          email: emailRef.current.value,
-          password: passwordRef.current.value,
+        await axios.post<User>(`${NEXT_API_URL}/party`, {
+          name: nameRef.current.value,
         })
-        alert('Sigup success')
+        alert('Create party success')
         router.replace('/')
       } catch (error) {
-        alert('Sigup fail')
+        alert('Create party fail')
       }
     }
   }
@@ -30,7 +28,7 @@ const Signup = () => {
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign Up
+            Create party
           </h2>
         </div>
 
@@ -38,39 +36,16 @@ const Signup = () => {
           <form action="#" className="space-y-6" method="POST">
             <div>
               <label className="block text-sm font-medium leading-6 text-gray-900" htmlFor="email">
-                Email
+                Name
               </label>
               <div className="mt-2">
                 <input
-                  autoComplete="email"
                   className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                  id="email"
-                  name="email"
-                  ref={emailRef}
+                  id="name"
+                  name="name"
+                  ref={nameRef}
                   required
-                  type="email"
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                  htmlFor="password"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  autoComplete="current-password"
-                  className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600"
-                  id="password"
-                  name="password"
-                  ref={passwordRef}
-                  required
-                  type="password"
+                  type="text"
                 />
               </div>
             </div>
@@ -78,10 +53,10 @@ const Signup = () => {
             <div>
               <button
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                onClick={handleSignup}
+                onClick={handleParty}
                 type="button"
               >
-                Sign Up
+                Create Party
               </button>
             </div>
           </form>
@@ -91,4 +66,4 @@ const Signup = () => {
   )
 }
 
-export default Signup
+export default Createparty
